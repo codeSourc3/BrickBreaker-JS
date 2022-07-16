c9c17c36f6c6a1c6786cfc7294c2fd56a36ff4e6
Enzo M <enzojmayo@gmail.com>
2022-07-16 13:42:01 -0500

    Refactored event emitter

--- utils.js
+++ utils.js
@@ -5,4 +5,16 @@ class Logger {
     }
 }
 
-export {Logger};
+function template(strings, ...keys) {
+    return (...values) => {
+        let dict = values[values.length - 1] || [];
+        let result = [strings[0]];
+        keys.forEach((key, index) => {
+            let value = Number.isFinite(key) ? values[key] : dict[key];
+            result.push(value, strings[index + 1]);
+        });
+        return result.join('');
+    };
+}
+
+export {Logger, template};
