2140fb2fa87e41a1f0f03a13261ff6e489642a0c
Enzo M <enzojmayo@gmail.com>
2022-07-25 22:00:20 -0500

    Add methods to vec2.js

--- math/vec2.js
+++ math/vec2.js
@@ -9,9 +9,45 @@ export class Vec2 {
      * @param {Vec2} vec 
      * @param {number} scaleFactor 
      */
-    static scale(vec, scaleFactor) {
+    static multiplyScalar(vec, scaleFactor) {
         vec.x *= scaleFactor;
         vec.y *= scaleFactor;
+        return vec;
+    }
+
+    /**
+     * 
+     * @param {Vec2} vec 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    static addScalar(vec, scalar) {
+        vec.x += scalar;
+        vec.y += scalar;
+        return vec;
+    }
+
+    /**
+     * 
+     * @param {Vec2} vec 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    static minusScalar(vec, scalar) {
+        vec.x -= scalar;
+        vec.y -= scalar;
+        return vec;
+    }
+
+    /**
+     * 
+     * @param {Vec2} vec 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    static divideByScalar(vec, scalar) {
+        vec.x /= scalar;
+        vec.y /= scalar;
         return vec;
     }
 
@@ -58,13 +94,68 @@ export class Vec2 {
     }
 
     /**
-     * Scales the vector by the scale factor.
-     * @param {number} scaleFactor the factor to scale by.
-     * @returns {Vec2} the new vector.
+     * 
+     * @param {Vec2} vec1 
+     * @param {Vec2} vec2 
+     * @returns {Vec2}
      */
-    scale(scaleFactor) {
-        return new Vec2(this.x * scaleFactor, this.y * scaleFactor);
+    static multiply(vec1, vec2) {
+        vec1.x *= vec2.x;
+        vec1.y *= vec2.y;
+        return vec1;
     }
+
+    /**
+     * 
+     * @param {Vec2} vec1 
+     * @param {Vec2} vec2 
+     * @returns {Vec2}
+     */
+    static divide(vec1, vec2) {
+        vec1.x /= vec2.x;
+        vec1.y /= vec2.y;
+        return vec1;
+    }
+    
+    /**
+     * 
+     * @param {Vec2} vec1 
+     * @param {Vec2} vec2 
+     * @returns {boolean}
+     */
+    static equals(vec1, vec2) {
+        return vec1.x === vec2.x && vec1.y === vec2.y;
+    }
+
+    /**
+     * 
+     * @param {Vec2} vec1 
+     * @param {Vec2} vec2 
+     * @returns {number}
+     */
+    static dot(vec1, vec2) {
+        return vec1.x * vec2.x + vec1.y * vec2.y;
+    }
+
+    /**
+     * 
+     * @param {Vec2} vector the vector to find the length for.
+     * @returns {number}
+     */
+    static length(vector) {
+        return Math.sqrt(Vec2.dot(vector, vector));
+    }
+
+    /**
+     * 
+     * @param {Vec2} vec1 
+     * @param {Vec2} vec2 
+     * @param {number} fraction 
+     */
+    static lerp(vec1, vec2, fraction) {
+        return vec2.minus(vec1).multiplyScalar(fraction).add(vec1);
+    }
+
 
     /**
      * Adds a vector to the existing vector.
@@ -85,6 +176,52 @@ export class Vec2 {
     }
 
     /**
+     * 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    addScalar(scalar) {
+        this.x += scalar;
+        this.y += scalar;
+        return this;
+    }
+
+    /**
+     * 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    minusScalar(scalar) {
+        this.x -= scalar;
+        this.y -= scalar;
+        return this;
+    }
+
+    /**
+     * 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    multiplyScalar(scalar) {
+        this.x *= scalar;
+        this.y *= scalar;
+        return this;
+    }
+
+    /**
+     * 
+     * @param {number} scalar 
+     * @returns {Vec2}
+     */
+    divideByScalar(scalar) {
+        this.x /= scalar;
+        this.y /= scalar;
+        return this;
+    }
+
+
+
+    /**
      * Inverts the vector.
      * @returns {Vec2} the inverted vector.
      */
@@ -101,12 +238,64 @@ export class Vec2 {
     }
 
     /**
-     * Gets the distance from the other vector;
-     * @param {Vec2} vector the other vector to measure the distance from.
-     * @returns {number} the distance from the other vector.
+     * Calculates the length of the vector.
+     * @returns {number} the length of the vector.
      */
-    distance(vector) {
-        return Math.sqrt(Math.pow(vector.x - this.x, 2) + Math.pow(vector.y - this.y, 2));
+    get length() {
+        return Math.sqrt(this.dot(this));
+    }
+
+    /**
+     * Finds the unit vector of the current vector.
+     * 
+     * @returns {Vec2} the unit vector.
+     */
+    get unit() {
+        return this.divideByScalar(this.length);
+    }
+
+    /**
+     * 
+     * @param {Vec2} vector 
+     * @returns {Vec2}
+     */
+    multiply(vector) {
+        return new Vec2(this.x * vector.x, this.y * vector.y);
+    }
+
+    /**
+     * 
+     * @param {Vec2} vector another vector
+     * @returns {Vec2}
+     */
+    divide(vector) {
+        return new Vec2(this.x / vector.x, this.y / vector.y);
+    }
+
+    /**
+     * 
+     * @param {Vec2} vector 
+     * @returns {boolean}
+     */
+    equals(vector) {
+        return this.x === vector.x && this.y === vector.y;
+    }
+
+    /**
+     * Returns the dot product of a vector.
+     * @param {Vec2} vector 
+     * @returns {number}
+     */
+    dot(vector) {
+        return this.x * vector.x + this.y * vector.y;
+    }
+
+    /**
+     * 
+     * @param {Vec2} vector the other vector to find the angle to.
+     */
+    angleTo(vector) {
+        return Math.acos(this.dot(vector) / (this.length * vector.length));
     }
 
     /**
@@ -115,5 +304,9 @@ export class Vec2 {
      */
     clone() {
         return new Vec2(this.x, this.y);
+    }
+
+    toString() {
+        return `X: ${this.x}, Y: ${this.y}`;
     }
 } 
